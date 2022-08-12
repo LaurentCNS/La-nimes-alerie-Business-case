c88ebe66d6275ea93622eb501b29fe5c42c7b990
Laurent cns <lo42fr@gmail.com>
2022-08-12 15:45:45 +0200

    ajout des messages flash avec capture d'esception

--- src/Controller/back/AdminCategorieController.php
+++ src/Controller/back/AdminCategorieController.php
@@ -64,7 +64,12 @@ class AdminCategorieController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            try{
             $categorieRepository->add($categorie, true);
+            $this->addFlash('success', 'La catégorie a bien été ajoutée');
+            }catch (\Exception $e){
+                $this->addFlash('danger', 'La catégorie n\'a pas été ajoutée');
+            }
 
             return $this->redirectToRoute('app_admin_categorie_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -90,7 +95,12 @@ class AdminCategorieController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            try{
             $categorieRepository->add($categorie, true);
+            $this->addFlash('success', 'La catégorie a bien été modifiée');
+            }catch (\Exception $e){
+                $this->addFlash('danger', 'La catégorie n\'a pas été modifiée');
+            }
 
             return $this->redirectToRoute('app_admin_categorie_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -105,7 +115,13 @@ class AdminCategorieController extends AbstractController
     public function delete(Request $request, Categorie $categorie, CategorieRepository $categorieRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$categorie->getId(), $request->request->get('_token'))) {
-            $categorieRepository->remove($categorie, true);
+            // if it's a delete request the entity is removed from the repository else catch the exception
+            try {
+                $categorieRepository->remove($categorie, true);
+                $this->addFlash('success', 'Opération correctement effectuée.');
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'Impossible de supprimer, une relation est présente avec d\'autres tables.');
+            }
         }
 
         return $this->redirectToRoute('app_admin_categorie_index', [], Response::HTTP_SEE_OTHER);
--- src/Repository/CategorieRepository.php
+++ src/Repository/CategorieRepository.php
@@ -94,4 +94,5 @@ class CategorieRepository extends AbstractLanimalerieRepository
         ;
     }
 
+
 }
