fdc3ee1b455ea8d7e6649804405379f8575ad7ae
Laurent cns <lo42fr@gmail.com>
2022-08-27 09:58:10 +0200

    mise en place des avis pour detail produit

--- src/Controller/front/AddFavoriteController.php
+++ src/Controller/front/AddFavoriteController.php
@@ -11,11 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AddFavoriteController extends AbstractController
 {
-    #[Route('/add/favorite/{id}', name: 'app_add_favorite')]
-    public function index($id, EntityManagerInterface $entityManager, Produit $produit): Response
+    #[Route('/add/favorite/{slug}', name: 'app_add_favorite')]
+    public function index($slug, EntityManagerInterface $entityManager, Produit $produit): Response
     {
+
 
         return $this->redirectToRoute('app_produit');
 
     }
+
 }
